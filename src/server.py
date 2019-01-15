aad21faac1a34a38cfd4366d840dc6ad1876093c
Julio JS. Sanchez <automatizacion@armaselaceros.com>
2019-01-15 11:13:16 -0600

    form submit with email and password

--- src/server.py
+++ src/server.py
@@ -7,11 +7,15 @@ app.env = "development"
 @app.route("/")
 def form():
     return render_template("form.html")
-    
+
 @app.route("/table", methods=["POST"])
 def table():
     email = request.form.get("email1")
-    return render_template("table.html", name=email)
+    password = request.form.get("password1")
+    if email == "julio.sanchez@armasel.com" and password == "Autom2018":
+        return render_template("table.html")
+    else:
+        return "Datos incorrectos, regrese a la página de inicio para volver a intentarlo."
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
