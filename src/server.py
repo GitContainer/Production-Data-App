c273a5e8de92cf7796d0ea3c1472b16d4c99da2c
Julio JS. Sanchez <automatizacion@armaselaceros.com>
2019-01-15 09:02:37 -0600

    Changed folder structure

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 app.debug = True
@@ -6,12 +7,7 @@ app.env = "development"
 
 @app.route("/")
 def index():
-    return "Hello, world!!!"
-
-@app.route("/<string:name>")
-def hello(name):
-    name = name.capitalize()
-    return "Hello " + name + "!"
+    return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
