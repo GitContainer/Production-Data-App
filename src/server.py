edfe58552d0e025f15f867f1bff9fd21d059c0a5
Julio JS. Sanchez <automatizacion@armaselaceros.com>
2019-01-15 10:29:10 -0600

    Linked form and table

--- src/server.py
+++ src/server.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -8,6 +7,11 @@ app.env = "development"
 @app.route("/")
 def form():
     return render_template("form.html")
+    
+@app.route("/table", methods=["POST"])
+def table():
+    email = request.form.get("email1")
+    return render_template("table.html", name=email)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
